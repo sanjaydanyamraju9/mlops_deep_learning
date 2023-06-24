b5502ca6961962e3e0b31c670881e33356b58855
sanjaydanyamraju9 <danyamraju.sanjaybhargav@gmail.com>
2023-06-24 17:26:45 -0500

    project template using cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='END to END DL deployment on azure and aws using CI/CD pipelines',
+    author='sanjay_danyamraju',
+    license='MIT',
+)
